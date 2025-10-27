ca7d5010dc364620ea74ea21790f48a74b9969cc
NihalAgarwall <dasahing02497@gmail.com>
2025-10-27 14:34:59 +0530

    Fix SmoothScroll Lenis config

--- components/layout/SmoothScroll.tsx
+++ components/layout/SmoothScroll.tsx
@@ -1,15 +1,15 @@
 'use client';
 
-import { useEffect, ReactNode } from 'react';
-import Lenis from 'lenis';
+import { useEffect } from 'react';
+import Lenis from '@studio-freight/lenis';
 
-export default function SmoothScroll({ children }: { children: ReactNode }) {
+export default function SmoothScroll({ children }: { children: React.ReactNode }) {
   useEffect(() => {
     const lenis = new Lenis({
       duration: 1.2,
       easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
       smoothWheel: true,
-      smoothTouch: false,
+      touchMultiplier: 2,
     });
 
     function raf(time: number) {
